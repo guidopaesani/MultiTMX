e75849c5d9db65cd724b41e0a953122d87e55339
Guido Paesani <guidopaesani@gmail.com>
2016-06-07 17:10:14 -0300

    Initial version

---
+++ MultiTMX-2-MonoTXTs/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace MultiTMX_2_MonoTXTs
+{
+    partial class MultiTMX2MonoTXTs
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MultiTMX2MonoTXTs));
+            this.TxBxInputFile = new System.Windows.Forms.TextBox();
+            this.startAsyncButton = new System.Windows.Forms.Button();
+            this.BkGdWorker = new System.ComponentModel.BackgroundWorker();
+            this.cancelAsyncButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.btnInputFile = new System.Windows.Forms.Button();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.DescLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxBxInputFile
+            // 
+            this.TxBxInputFile.Location = new System.Drawing.Point(97, 34);
+            this.TxBxInputFile.Name = "TxBxInputFile";
+            this.TxBxInputFile.Size = new System.Drawing.Size(422, 20);
+            this.TxBxInputFile.TabIndex = 0;
+            this.TxBxInputFile.Text = "C:\\_WORK\\_Customers\\Amazon\\AWS\\AWS_TM.tmx";
+            this.TxBxInputFile.TextChanged += new System.EventHandler(this.TxBxInputFile_TextChanged);
+            // 
+            // startAsyncButton
+            // 
+            this.startAsyncButton.Location = new System.Drawing.Point(363, 63);
+            this.startAsyncButton.Name = "startAsyncButton";
+            this.startAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.startAsyncButton.TabIndex = 1;
+            this.startAsyncButton.Text = "Start";
+            this.startAsyncButton.UseVisualStyleBackColor = true;
+            this.startAsyncButton.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // BkGdWorker
+            // 
+            this.BkGdWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BkGdWorker_DoWork);
+            this.BkGdWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.BkGdWorker_ProgressChanged);
+            this.BkGdWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BkGdWorker_RunWorkerCompleted);
+            // 
+            // cancelAsyncButton
+            // 
+            this.cancelAsyncButton.Location = new System.Drawing.Point(444, 63);
+            this.cancelAsyncButton.Name = "cancelAsyncButton";
+            this.cancelAsyncButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelAsyncButton.TabIndex = 2;
+            this.cancelAsyncButton.Text = "Cancel";
+            this.cancelAsyncButton.UseVisualStyleBackColor = true;
+            this.cancelAsyncButton.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(157, 96);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(35, 13);
+            this.resultLabel.TabIndex = 3;
+            this.resultLabel.Text = "status";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 92);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(139, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // btnInputFile
+            // 
+            this.btnInputFile.Location = new System.Drawing.Point(13, 34);
+            this.btnInputFile.Name = "btnInputFile";
+            this.btnInputFile.Size = new System.Drawing.Size(75, 23);
+            this.btnInputFile.TabIndex = 5;
+            this.btnInputFile.Text = "Input file...";
+            this.btnInputFile.UseVisualStyleBackColor = true;
+            this.btnInputFile.Click += new System.EventHandler(this.btnInputFile_Click);
+            // 
+            // DescLabel
+            // 
+            this.DescLabel.AutoSize = true;
+            this.DescLabel.Location = new System.Drawing.Point(12, 9);
+            this.DescLabel.Name = "DescLabel";
+            this.DescLabel.Size = new System.Drawing.Size(298, 13);
+            this.DescLabel.TabIndex = 6;
+            this.DescLabel.Text = "Extraction of multilingual TMX segments to individual TXT files";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Label1";
+            // 
+            // MultiTMX2MonoTXTs
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 118);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DescLabel);
+            this.Controls.Add(this.btnInputFile);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.cancelAsyncButton);
+            this.Controls.Add(this.startAsyncButton);
+            this.Controls.Add(this.TxBxInputFile);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MultiTMX2MonoTXTs";
+            this.Text = "TMX to Monolingual TXTs";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxBxInputFile;
+        private System.Windows.Forms.Button startAsyncButton;
+        private System.ComponentModel.BackgroundWorker BkGdWorker;
+        private System.Windows.Forms.Button cancelAsyncButton;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button btnInputFile;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label DescLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ MultiTMX-2-MonoTXTs/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace MultiTMX_2_MonoTXTs
+{
+    public partial class MultiTMX2MonoTXTs : Form
+    {
+        public MultiTMX2MonoTXTs()
+        {
+            InitializeComponent();
+            BkGdWorker.WorkerReportsProgress = true;
+            BkGdWorker.WorkerSupportsCancellation = true;
+            progressBar1.Visible = false;
+            DescLabel.Text = "From a TMX, extract segments to individual, monolingual TXT files";
+            label1.Text = "";
+            resultLabel.Text = "Select a TMX and click Start";
+            // TxBxInputFile.Text = @"C:\_WORK\_Customers\Facebook\TMXs\Facebook_es_ES.tmx";
+            TxBxInputFile.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        }
+
+        static Dictionary<string, List<string>> StreamMultilingualTMX(string uri)
+        {   
+            Dictionary<string, List<string>> TXTs = new Dictionary<string, List<string>>();
+            List<string> Segments;
+
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.CheckCharacters = false;
+            settings.DtdProcessing = DtdProcessing.Ignore;
+
+            using (XmlReader reader = XmlReader.Create(uri, settings))
+            //using (XmlReader reader = XmlReader.Create(uri))
+            {
+                XElement header = null; 
+                XElement tuv = null;
+                //int vCurrentLine = 0;
+                reader.MoveToContent();
+                //vCurrentLine++;
+                // loop through elements
+                while (reader.Read())
+                {
+                    //vCurrentLine++;
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "header")
+                    {
+                        header = XElement.ReadFrom(reader) as XElement;
+                    }
+
+                    if (reader.NodeType == XmlNodeType.Element && reader.Name == "tuv")
+                    {
+                        string vLang = "UNKNOWN";
+                        string vSegment = "ERRROR";
+                        tuv = XElement.ReadFrom(reader) as XElement;
+                        if(tuv.HasAttributes==true)
+                        {
+                            foreach (XAttribute vAttr in tuv.Attributes())
+                            {
+                                if (vAttr.Name.LocalName=="lang") 
+                                {
+                                    vLang = vAttr.Value;
+                                }
+                            }
+                        }
+                        if(tuv.HasElements==true)
+                        {
+                            foreach (XElement vElement in tuv.Elements())
+                            {
+                                if (vElement.Name.LocalName=="seg")
+                                {
+                                    vSegment = vElement.Value;
+                                }
+                            }
+                        }
+                        if (!TXTs.TryGetValue(vLang, out Segments))
+                        {
+                            Segments = new List<string>();
+                            TXTs[vLang] = Segments;
+                        }
+                        Segments.Add(vSegment);
+                    }
+                    //worker.ReportProgress(vCurrentLine / vTotalLines * 100);
+                }
+            }
+            return TXTs;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (BkGdWorker.IsBusy != true)
+            {
+                // Start the asynchronous operation.
+                progressBar1.Visible = true; 
+                resultLabel.Text = "In progress...";
+                progressBar1.Value = 0;
+                timer.Enabled = true;
+                timer.Tick += new EventHandler(timer_Tick);
+                // Sets the timer interval to X seconds (milisecs).
+                timer.Interval = 2000;
+                timer.Start();
+                BkGdWorker.RunWorkerAsync();
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e) 
+        {
+            progressBar1.Value += 1;
+            if (progressBar1.Value > 50)
+                progressBar1.Value = 50;
+            resultLabel.Text = ("In progress..." + progressBar1.Value + "%");
+        }
+
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            if (BkGdWorker.WorkerSupportsCancellation == true)
+            {
+                // Cancel the asynchronous operation.
+                BkGdWorker.CancelAsync();
+            }
+        }
+
+        // This event handler is where the time-consuming work is done. 
+        private void BkGdWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+                if (worker.CancellationPending == true)
+                {
+                    e.Cancel = true;
+                    //break;
+                }
+                else
+                {
+                    // Perform a time consuming operation and report progress.
+                    Dictionary<string, List<string>> TXTs = StreamMultilingualTMX(TxBxInputFile.Text);
+                    worker.ReportProgress(50);
+                    timer.Stop();
+                    int i = 0;
+                    foreach (string vLanguage in TXTs.Keys)
+                    {
+                        string vFilename = Path.GetDirectoryName(TxBxInputFile.Text) + @"\" + Path.GetFileNameWithoutExtension(TxBxInputFile.Text) + "_" + vLanguage + ".txt";
+                        File.WriteAllLines(vFilename, TXTs[vLanguage]);
+                        i++;
+                        System.Threading.Thread.Sleep(500);
+                        int vProgress = Convert.ToInt32(((Decimal.Divide(i, TXTs.Keys.Count)) * 50) + 50);
+                        worker.ReportProgress(vProgress);
+                    }
+
+
+                }
+            //}
+        }
+
+        // This event handler updates the progress. 
+        private void BkGdWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            resultLabel.Text = ("In progress..." + e.ProgressPercentage.ToString() + "%");
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        // This event handler deals with the results of the background operation. 
+        private void BkGdWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Cancelled == true)
+            {
+                resultLabel.Text = "Canceled!";
+            }
+            else if (e.Error != null)
+            {
+                resultLabel.Text = "Error: " + e.Error.Message;
+            }
+            else
+            {
+                resultLabel.Text = "Done!";
+            }
+        }
+
+        private void btnInputFile_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            openFileDialog1.Filter = "TMX Files (.tmx)|*.tmx|All Files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 1;
+            if (result == DialogResult.OK) // Test result.
+            {
+                TxBxInputFile.Text = openFileDialog1.FileName;
+            }
+            Console.WriteLine(result); // <-- For debugging use.
+        }
+
+        private void TxBxInputFile_TextChanged(object sender, EventArgs e)
+        {
+            if (TxBxInputFile.Text=="") return;
+            if (File.Exists(TxBxInputFile.Text))
+            {
+                label1.Text = "File exists.";
+            } else {
+                label1.Text = "File does not exist.";
+                return;
+            }
+            //count lines and test parsing
+            int vTotalLines = 0;
+            XmlReaderSettings settings = new XmlReaderSettings();
+            settings.CheckCharacters = false;
+            settings.DtdProcessing = DtdProcessing.Ignore;
+
+            using (XmlReader reader = XmlReader.Create(TxBxInputFile.Text, settings))
+            //using (XmlReader reader = XmlReader.Create(TxBxInputFile.Text))
+            {
+                //reader.MoveToContent();
+                //Encoding.Default.GetString(Encoding.UTF8.GetBytes(reader.);
+                try
+                {
+                    while (reader.Read())
+                    {
+                        vTotalLines++;
+                    }
+                }
+                catch (Exception Ex)
+                {
+                    label1.Text = Ex.Message;
+                    //Regex.Replace(reader., @"[^\w\.@-]", "", RegexOptions.None, TimeSpan.FromSeconds(1.5));
+                }
+            }
+        }
+    }
+}
